79883f73aff2c799170613b6bd2085992936444a
batuhannoz <b.ozdmr23@gmail.com>
2024-02-29 20:08:20 +0300

    Add app icons

---
+++ app/src/main/java/com/duzce/spotinotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.duzce.spotinotes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
