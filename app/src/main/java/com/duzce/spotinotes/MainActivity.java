5d1feaab90c39a0571aeeea9218bff6c3c5b7f75
batuhannoz <b.ozdmr23@gmail.com>
2024-03-06 17:20:21 +0300

    Bug fix

--- app/src/main/java/com/duzce/spotinotes/MainActivity.java
+++ app/src/main/java/com/duzce/spotinotes/MainActivity.java
@@ -1,14 +1,45 @@
 package com.duzce.spotinotes;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.duzce.spotinotes.ui.Profile;
+import com.duzce.spotinotes.ui.SavedNotes;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_main);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main,new SavedNotes()).commit();
+        navView = findViewById(R.id.nav_view);
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                if (item.getItemId() == R.id.navigation_saved_notes) {
+                    selectedFragment = new SavedNotes();
+                } else if (item.getItemId() == R.id.navigation_profile) {
+                    selectedFragment = new Profile();
+                }
 
+                if (selectedFragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.nav_host_fragment_activity_main, selectedFragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/duzce/spotinotes/ui/Profile.java
@@ -0,0 +1,20 @@
+package com.duzce.spotinotes.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.duzce.spotinotes.R;
+
+public class Profile extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
