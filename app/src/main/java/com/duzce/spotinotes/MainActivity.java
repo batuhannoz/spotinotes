f170b59fdc131f3061efb14d74c1d41f458fe6fa
batuhannoz <b.ozdmr23@gmail.com>
2024-03-06 17:20:21 +0300

    Add navigation functionality

--- app/src/main/java/com/duzce/spotinotes/MainActivity.java
+++ app/src/main/java/com/duzce/spotinotes/MainActivity.java
@@ -1,14 +1,39 @@
 package com.duzce.spotinotes;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.duzce.spotinotes.ui.Profile;
+import com.duzce.spotinotes.ui.SavedNotes;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navView;
+    Fragment mainFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit_main);
 
-
+        // find nav_view and add navigation functionality
+        navView = findViewById(R.id.nav_view);
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_saved_notes:
+                        mainFragment = new SavedNotes();
+                    case R.id.navigation_profile:
+                        mainFragment = new Profile();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, mainFragment).commit();
+                return true;
+            }
+        });
     }
 }
