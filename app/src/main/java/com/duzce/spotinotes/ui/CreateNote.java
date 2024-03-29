39d4dd80b9df09fb101ea130dec79e999c42513b
batuhannoz <b.ozdmr23@gmail.com>
2024-03-29 14:29:47 +0300

    Bug Fix close dialog when note created

--- app/src/main/java/com/duzce/spotinotes/ui/CreateNote.java
+++ app/src/main/java/com/duzce/spotinotes/ui/CreateNote.java
@@ -60,6 +60,7 @@ public class CreateNote extends DialogFragment {
                         note,
                         ""
                 ));
+                getDialog().dismiss();
                 Toast.makeText(getContext(), "Note Created", Toast.LENGTH_SHORT).show();
             } else if (player.currentlyPlayingType == CurrentlyPlayingType.Episode) {
                 Episode e = (Episode) player.currentlyPlayingItem;
@@ -75,6 +76,7 @@ public class CreateNote extends DialogFragment {
                         note,
                         ""
                 ));
+                getDialog().dismiss();
                 Toast.makeText(getContext(), "Note Created", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getContext(), "No Active Track", Toast.LENGTH_SHORT).show();
